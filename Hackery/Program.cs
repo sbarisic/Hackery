3873be4c71599a680d225d1165313862f95496c4
Saša Barišić <cartman300@net.hr>
2015-08-20 19:40:29 +0200

    Project files

---
+++ Hackery/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackery {
+	class Program {
+		static void Main(string[] args) {
+			Console.Title = "Hackery";
+
+			Console.WriteLine("Done!");
+			Console.ReadLine();
+		}
+	}
+}
